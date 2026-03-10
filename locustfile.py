843b3ffc5a282b1ac518010d07c23731bb625a1b
Mykola Radionov <apelsinbanan07@gmail.com>
2026-03-10 21:19:27 +0200

    FINISH: Scalable cluster with full monitoring stack and Locust v2

--- locustfile.py
+++ locustfile.py
@@ -1,16 +1,10 @@
 from locust import HttpUser, task, between
-import random 
-class MyBot(HttpUser):
-    wait_time = between(0.5, 1.0)
-    @task(1)
-    def load_main_page(self):
-        self.client.get("/")
+import random
 
-    @task(3)
-    def fill_form(self):
-        bot_number = random.randint(1000, 99999)
-        fake_name = f"Bot_Number_{bot_number}"
-        self.client.post("/submit", data= {
-            "name": fake_name,
-            "eamil": f"bot{bot_number}@hacker.com"
-        })
+class MyBot(HttpUser):
+    wait_time = between(0.1, 0.2) 
+
+    @task
+    def attack_fastapi(self):
+        fake_name = f"Bot_User_{random.randint(1, 100000)}"
+        self.client.get(f"/?name={fake_name}")
